f136738296f3a4823ccaa1a4f42da57428a60720
Aaron Amodt <aaron@amodttech.com>
2020-09-07 21:23:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
 board = ["  ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
-rows = [" #{board[0]} | #{board[1]} | #{board[2]} ", "-----------", " #{board[3]} | #{board[4]} | #{board[5]} ", "-----------"," #{board[6]} | #{board[7]} | #{board[8]} "]
+
 
 def display_board(board)
   puts rows[0]
