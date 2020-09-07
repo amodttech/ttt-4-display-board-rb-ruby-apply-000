0115e3f92c68cd4757cb21bb3b3e1741ed158de3
Aaron Amodt <aaron@amodttech.com>
2020-09-07 21:03:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,15 +1,11 @@
 board = ["  ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
-rows = ["   |   |   ", "-----------", "   |   |   ", "-----------","   |   |   "]
-
-
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts "#{board[3]}|#{board[4]}|#{board[5]}"
-  puts "-----------"
-  puts "#{board[6]}|#{board[7]}|#{board[8]}"
-end
+rows = [" #{board[0]} | #{board[1]} | #{board[2]} ", "-----------", " #{board[3]} | #{board[4]} | #{board[5]} ", "-----------"," #{board[6]} | #{board[7]} | #{board[8]} "]
 
 def display_board(board)
   puts rows[0]
+  puts rows[1]
+  puts rows[2]
+  puts rows[3]
+  puts rows[4]
+  puts rows[5]
+end
